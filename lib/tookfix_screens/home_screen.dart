ef1679d112aa524d552af7a42d7029806b150aeb
kimbomm <bomm@heybit.io>
2024-03-25 23:01:18 +0900

    #6 Webtoon app
    6.8 Webtoon Card

--- lib/tookfix_screens/home_screen.dart
+++ lib/tookfix_screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:toonfix/models/webtoon_model.dart';
 import 'package:toonfix/services/api_services.dart';
 
@@ -27,22 +29,67 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                print(index);
-                return Text(snapshot.data![index].title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 30.0,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    offset: const Offset(10, 10),
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+              child: Image.network(
+                snapshot.data![index].thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              snapshot.data![index].title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40.0,
       ),
     );
   }
